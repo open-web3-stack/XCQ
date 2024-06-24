17223f83101450af0b6cb852313e683e0e6983a3
indirection42 <jiyuanz95@gmail.com>
2024-06-24 17:48:03 +0800

    improve type safety

--- xcq-extension/procedural/src/lib.rs
+++ xcq-extension/procedural/src/lib.rs
@@ -1,128 +1,130 @@
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, spanned::Spanned};
+use syn::token::Comma;
+use syn::{parse_macro_input, parse_quote, spanned::Spanned};
+use syn::{punctuated::Punctuated, ExprCall, Field, Ident, ItemImpl, LitInt, Pat, TraitItem, Variant};
 
 #[derive(Clone)]
 struct Method {
     /// Function name
-    pub name: syn::Ident,
+    pub name: Ident,
     /// Information on args: `(name, type)`
-    pub args: Vec<(syn::Ident, Box<syn::Type>)>,
+    pub args: Vec<(Ident, Box<syn::Type>)>,
 }
 
 #[proc_macro_attribute]
 pub fn extension(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as syn::ItemTrait);
-    let trait_name = &input.ident;
-    let trait_items = &input.items;
 
-    let methods = match methods(trait_items) {
+    let methods = match methods(&input.items) {
         Ok(method) => method,
         Err(e) => return e.to_compile_error().into(),
     };
 
-    // TODO: add phantom data if Impl is not used
-    let variants_replace_generics = match methods
-        .iter()
-        .map(|method| {
-            let name = &method.name;
-            let args = method
-                .args
-                .iter()
-                .map(|(name, ty)| {
-                    let ty = replace_self_to_impl(ty)?;
-                    Ok(quote! {
-                        #name: #ty
-                    })
-                })
-                .collect::<syn::Result<Vec<_>>>()?;
-            Ok(quote! {
-                #name {
-                    #(#args),*
-                }
-            })
-        })
-        .collect::<syn::Result<Vec<_>>>()
-    {
-        Ok(variants) => variants,
+    let call_enum_def = match call_enum_def(&input.ident, &methods) {
+        Ok(call_enum_def) => call_enum_def,
         Err(e) => return e.to_compile_error().into(),
     };
 
-    let variants_no_arg_type = methods
-        .iter()
-        .map(|method| {
-            let name = &method.name;
-            let args = method.args.iter().map(|(name, _ty)| {
-                quote! {
-                    #name
-                }
-            });
-            quote! {
-                #name {
-                    #(#args),*
-                }
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let methods_no_arg_type = methods
-        .iter()
-        .map(|method| {
-            let name = &method.name;
-            let args = method.args.iter().map(|(name, _ty)| {
-                quote! {
-                    #name
-                }
-            });
-            quote! {
-                #name (
-                    #(#args),*
-                )
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let call_def = quote! {
-        #[derive(Decode)]
-        pub enum Call<Impl: #trait_name> {
-            #(
-                #[allow(non_camel_case_types)]
-                #variants_replace_generics
-            ),*
-        }
-    };
-
-    let dispatchable_impl = quote! {
-        impl<Impl: #trait_name> xcq_extension::Dispatchable for Call<Impl> {
-            fn dispatch(self) -> Result<Vec<u8>, xcq_extension::DispatchError> {
-                match self {
-                    #( Self::#variants_no_arg_type => Ok(Impl::#methods_no_arg_type.encode())),*
-                }
-            }
-        }
-    };
-
-    // TODO calculate according to trait definition
-    // Currently, specify the extension_id as `args`
-    let extension_id = syn::parse_macro_input!(args as syn::LitInt);
-    let extension_id_impl = quote! {
-        impl<Impl: #trait_name> xcq_extension::ExtensionId for Call<Impl> {
-            const EXTENSION_ID: xcq_extension::ExtensionIdTy = #extension_id;
-        }
-    };
+    let dispatchable_impl = dispatchable_impl(&input.ident, &methods);
+    let extension_id = parse_macro_input!(args as LitInt);
+    let extension_id_impl = extension_id_impl(&input.ident, &extension_id);
 
     let expanded = quote! {
         #input
-        #call_def
+        #call_enum_def
         #dispatchable_impl
         #extension_id_impl
     };
     expanded.into()
 }
 
-fn methods(trait_items: &Vec<syn::TraitItem>) -> syn::Result<Vec<Method>> {
+fn call_enum_def(trait_ident: &Ident, methods: &[Method]) -> syn::Result<syn::ItemEnum> {
+    // TODO: add phantom data if Impl is not used
+    let mut variants = Punctuated::<Variant, Comma>::new();
+    for method in methods {
+        let name = &method.name;
+        let mut args = Punctuated::<Field, Comma>::new();
+        for (name, ty) in &method.args {
+            let ty = replace_self_to_impl(ty)?;
+            args.push(parse_quote! {
+                #name: #ty
+            });
+        }
+        variants.push(parse_quote! {
+            #[allow(non_camel_case_types)]
+            #name {
+                #args
+            }
+        });
+    }
+    Ok(parse_quote!(
+        #[derive(Decode)]
+        pub enum Call<Impl: #trait_ident> {
+            #variants
+        }
+    ))
+}
+
+fn dispatchable_impl(trait_ident: &Ident, methods: &[Method]) -> TokenStream {
+    let mut pats = Vec::<Pat>::new();
+    for method in methods {
+        let name = &method.name;
+        let mut args = Punctuated::<Ident, Comma>::new();
+        for (ident, _ty) in &method.args {
+            args.push(parse_quote! {
+                #ident
+            });
+        }
+        pats.push(parse_quote! {
+            Self::#name {
+                #args
+            }
+        });
+    }
+
+    let mut method_calls = Vec::<ExprCall>::new();
+    for method in methods {
+        let name = &method.name;
+        let mut args = Punctuated::<Ident, Comma>::new();
+        for (ident, _ty) in &method.args {
+            args.push(parse_quote! {
+                #ident
+            });
+        }
+        method_calls.push({
+            parse_quote! {
+                Impl::#name(#args)
+            }
+        });
+    }
+
+    parse_quote! {
+        impl<Impl: #trait_ident> xcq_extension::Dispatchable for Call<Impl> {
+            fn dispatch(self) -> Result<Vec<u8>, xcq_extension::DispatchError> {
+                match self {
+                    #( #pats => Ok(#method_calls.encode())),*
+                }
+            }
+        }
+    }
+}
+
+fn extension_id_impl(trait_ident: &Ident, extension_id: &LitInt) -> ItemImpl {
+    // TODO calculate according to trait definition
+    // Currently, specify the extension_id as `args`
+    parse_quote! {
+        impl<Impl: #trait_ident> xcq_extension::ExtensionId for Call<Impl> {
+            const EXTENSION_ID: xcq_extension::ExtensionIdTy = #extension_id;
+        }
+    }
+}
+
+// helper functions
+fn methods(trait_items: &Vec<TraitItem>) -> syn::Result<Vec<Method>> {
     let mut methods = vec![];
     for item in trait_items {
-        if let syn::TraitItem::Fn(method) = item {
+        if let TraitItem::Fn(method) = item {
             let method_name = &method.sig.ident;
             let mut method_args = vec![];
             for arg in method.sig.inputs.iter() {
@@ -158,3 +160,35 @@ fn replace_self_to_impl(ty: &syn::Type) -> syn::Result<Box<syn::Type>> {
 
     Ok(Box::new(modified_ty))
 }
+
+// #[cfg(test)]
+// mod tests {
+// use super::*;
+// #[test]
+// fn methods_works() {
+//     let input: ItemTrait = parse_quote!(
+//         pub trait NoGenericsTrait {
+//             fn method1(a: u32) -> u32;
+//             fn method2(a: u32, b: u32) -> u32;
+//         }
+//     )
+//     .unwrap();
+//     let methods = methods(&input.items).unwrap();
+//     assert_eq!(methods.len(), 1);
+// assert_eq!(
+//     quote! {#(#methods),*},
+//     quote! { method1 {a: i32}, method2 {a:u32,b:u32}}
+// )
+// }
+
+// fn methods_rejects_self() {
+//     let input: ItemTrait = parse_quote!(
+//         pub trait TraitWithSelf {
+//             fn test(&self, a: u32) -> u32;
+//         }
+//     )
+//     .unwrap();
+//     let methods = methods(&input.items);
+//     assert!(methods.is_err());
+// }
+// }
